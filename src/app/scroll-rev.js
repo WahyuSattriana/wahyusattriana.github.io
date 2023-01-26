1f95adaaadf802ab056cf59f39af73a9609681b7
WahyuSattriana <wahyusattriana@gmail.com>
2023-01-26 12:52:42 +0800

    adding animation

---
+++ src/app/scroll-rev.js
@@ -0,0 +1,28 @@
+ScrollReveal().reveal('.landing-text', { 
+    delay: 300,
+    reset: false,
+    origin: 'bottom',
+    easing: 'ease-in-out',
+    distance: '50px'
+});
+ScrollReveal().reveal('.session-image, .skills', { 
+    delay: 300,
+    reset: false,
+    origin: 'bottom',
+    easing: 'ease-in-out',
+    distance: '50px'
+});
+ScrollReveal().reveal('.session-text', { 
+    delay: 700,
+    reset: false,
+    origin: 'bottom',
+    easing: 'ease-in-out',
+    distance: '50px'
+});
+ScrollReveal().reveal('.footer-container', { 
+    delay: 500,
+    reset: false,
+    origin: 'bottom',
+    easing: 'ease-in-out',
+    distance: '50px'
+});
